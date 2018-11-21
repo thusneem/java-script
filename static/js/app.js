2ea67fcf2e16ea129bc84f701bf749acbb654a20
Thusneem M <38304754+thusneem@users.noreply.github.com>
2018-11-21 00:01:32 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -16,30 +16,47 @@ tableData.forEach((ufodata) => {
 });
 
 var submit = d3.select("#filter-btn");
-// var inputs = document.getElementsByClassName('form-control');
-// console.log(inputs);
- submit.on("click", function() {
-   tbody.html(" ");
+submit.on("click", function() {
+tbody.html(" ");
+d3.event.preventDefault();
+   var filter = {};
+   filter.datetime = d3.select("#datetime").property("value");
+   //console.log(dateValue);
+   filter.city= d3.select("#city").property("value");
+   //console.log(cityValue);
+   filter.state = d3.select("#state").property("value");
+   //console.log(stateValue);
+   filter.country = d3.select("#country").property("value");
+   //console.log(countryValue);
+   filter.shape = d3.select("#shape").property("value");
+   //console.log(shapeValue);
+
+   console.log(filter);   
    
+     /**
+ * Filters an array of objects with multiple criteria.
+ *
+ * @param  {Array}  array: the array to filter
+ * @param  {Object} filters: an object with the filter criteria as the property names
+ * @return {Array}
+ */
+    const multiFilter = (arr, filters) => {
+        const filterKeys = Object.keys(filters);
+        return arr.filter(eachObj => {
+        return filterKeys.every(eachKey => {
+            if (!filters[eachKey].length) {
+            return true; // passing an empty filter means that filter is ignored.
+            }
+            return filters[eachKey].includes(eachObj[eachKey]);
+        });
+        });
+    };
+  var filtered = multiFilter(tableData,filter);
+  console.log(filtered)
    
-   d3.event.preventDefault();
-   var dateValue = d3.select("#datetime").property("value");
-   console.log(dateValue);
-   var cityValue = d3.select("#city").property("value");
-   console.log(cityValue);
-   var stateValue = d3.select("#state").property("value");
-   console.log(stateValue);
-   var countryValue = d3.select("#country").property("value");
-   console.log(countryValue);
-   var shapeValue = d3.select("#shape").property("value");
-   console.log(shapeValue);
-   
-  var filteredData = tableData.filter(ufo => ufo.datetime === dateValue || ufo.city === cityValue || ufo.state === stateValue||
-       ufo.country === countryValue||ufo.shape === shapeValue);
-   console.log(filteredData)
    
     
-    filteredData.forEach(ufosighting => {
+    filtered.forEach(ufosighting => {
       var row = tbody.append('tr');
       Object.entries(ufosighting).forEach(([key, value]) => {
       var cell = row.append("td");
